ed378c73003c1a46ff75516eebd693cfaa962e5a
Lawrence Chan <chanlawrencet@gmail.com>
2019-10-03 16:25:00 -0400

    lock unlock

--- appFrontend/App.js
+++ appFrontend/App.js
@@ -8,85 +8,39 @@ import {
     Alert,
 } from 'react-native';
 import Constants from 'expo-constants';
+import { Calculator } from 'react-native-calculator'
+import CalculatorMock from './components/CalculatorMock'
 
-function Separator() {
-    return <View style={styles.separator} />;
-}
 
-export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View>
-                <Text style={styles.title}>
-                    The title and onPress handler are required. It is recommended to set
-                    accessibilityLabel to help make your app usable by everyone.
-                </Text>
-                <Button
-                    title="Press me"
-                    onPress={() => Alert.alert('Simple Button pressed')}
-                />
-            </View>
-            <Separator />
-            <View>
-                <Text style={styles.title}>
-                    Adjust the color in a way that looks standard on each platform. On
-                    iOS, the color prop controls the color of the text. On Android, the
-                    color adjusts the backgroud color of the button.
-                </Text>
-                <Button
-                    title="Press me"
-                    color="#f194ff"
-                    onPress={() => Alert.alert('Button with adjusted color pressed')}
-                />
-            </View>
-            <Separator />
-            <View>
-                <Text style={styles.title}>
-                    All interaction for the component are disabled.
-                </Text>
-                <Button
-                    title="Press me"
-                    disabled
-                    onPress={() => Alert.alert('Cannot press this one')}
-                />
-            </View>
-            <Separator />
-            <View>
-                <Text style={styles.title}>
-                    This layout strategy lets the title define the width of the button.
-                </Text>
-                <View style={styles.fixToText}>
-                    <Button
-                        title="Left button"
-                        onPress={() => Alert.alert('Left button pressed')}
-                    />
-                    <Button
-                        title="Right button"
-                        onPress={() => Alert.alert('Right button pressed')}
-                    />
+
+class App extends React.Component{
+
+    unlock = () => {
+        this.setState({locked:false})
+    }
+
+    componentWillMount(){
+        this.setState({locked:true})
+    }
+
+    render(){
+        const {locked} = this.state
+
+        return(
+            <View style={{ flex: 1 }}>
+                {locked ? (
+                    <CalculatorMock locked unlock={this.unlock.bind(this)}/>
+                ) : 
+                <View>
+                    <Text style={{marginTop: 50, fontSize: 20, textAlign: "center"}}>Unnlocked!</Text>
+                    <Button onPress={() => this.setState({locked:true})} title='lock'/>
                 </View>
+
+                }
+                
             </View>
-        </SafeAreaView>
-    );
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: Constants.statusBarHeight,
-        marginHorizontal: 16,
-    },
-    title: {
-        textAlign: 'center',
-        marginVertical: 8,
-    },
-    fixToText: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    separator: {
-        marginVertical: 8,
-        borderBottomColor: '#737373',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-});
+export default App
