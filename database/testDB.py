90e383880548d9b21e54f688ff56793e39463666
Lawrence Chan <chanlawrencet@gmail.com>
2019-10-07 14:48:53 -0400

    uri update

--- database/testDB.py
+++ database/testDB.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
-MONGO_URL = os.environ.get('MONGOHQ_URL')
+MONGO_URL = os.environ.get('MONGODB_URI')
 app.config["MONGO_URI"] = MONGO_URL
 mongo = PyMongo(app)
 
