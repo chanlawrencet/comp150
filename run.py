3a08802efc9cf61a01d14f0ae2a6371cfbfb8a82
Lawrence Chan <chanlawrencet@gmail.com>
2019-10-31 19:58:16 -0400

    new endpoint

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def getImageIds():
         print('5')
         for photoID in userProfile['images']:
             print('6')
-            toReturn.append(photoID).toString()
+            toReturn.append(photoID.toString())
         print(toReturn)
         dict = {'dog': 1}
         return str({'imageIDs': toReturn})
