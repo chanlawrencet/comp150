7d66d00bb6b2ab36055091621e24ad31c494b35d
mrobin12 <mrobin12@gtufts.edu>
2019-10-31 20:38:32 -0400

    debugging backend

--- run.py
+++ run.py
@@ -29,90 +29,90 @@ def test():
     print(fs.get(a).read())
     return
 
-@app.route('/getImages', methods=['GET'])
-def getImages():
-    contents = list(db.forms.find())
+# @app.route('/getImages', methods=['GET'])
+# def getImages():
+#     contents = list(db.forms.find())
 
-    for content in contents:
-        if content['special'] == 'true':
-            imageBytes = fs.get(content['image']).read()
+#     for content in contents:
+#         if content['special'] == 'true':
+#             imageBytes = fs.get(content['image']).read()
 
-    print(type(imageBytes))
+#     print(type(imageBytes))
 
-    print('toReturn', imageBytes)
+#     print('toReturn', imageBytes)
 
-    return send_file(io.BytesIO(imageBytes),
-                     attachment_filename='logo.png',
-                     mimetype='image/png')
-
-
-@app.route('/getImageIDs', methods=['GET', 'POST'])
-def getImageIds():
-    userID = request.args.get('uid')
-    print('1')
-    if len(list(db.forms.find({"uid": userID}))) == 0:
-        print('2')
-        return str({'imageIDs': []})
-    else:
-        print('3')
-        userProfile = list(db.forms.find({"uid": userID}))[0]
-        print('4')
-        toReturn = []
-        print('5')
-        for photoID in userProfile['images']:
-            print('6')
-            toReturn.append(str(photoID))
-        print(toReturn)
-        dict = {'dog': 1}
-        return str({'imageIDs': toReturn})
+#     return send_file(io.BytesIO(imageBytes),
+#                      attachment_filename='logo.png',
+#                      mimetype='image/png')
 
 
+# @app.route('/getImageIDs', methods=['GET', 'POST'])
+# def getImageIds():
+#     userID = request.args.get('uid')
+#     print('1')
+#     if len(list(db.forms.find({"uid": userID}))) == 0:
+#         print('2')
+#         return str({'imageIDs': []})
+#     else:
+#         print('3')
+#         userProfile = list(db.forms.find({"uid": userID}))[0]
+#         print('4')
+#         toReturn = []
+#         print('5')
+#         for photoID in userProfile['images']:
+#             print('6')
+#             toReturn.append(str(photoID))
+#         print(toReturn)
+#         dict = {'dog': 1}
+#         return str({'imageIDs': toReturn})
 
-@app.route('/uploadImage', methods=['GET', 'POST'])
-def uploadImage():
-    userID = request.args.get('uid')
 
-    # retrieve file from request params
-    imagefile = request.files['image']
-    # retrieve filename from file
-    filename = werkzeug.utils.secure_filename(imagefile.filename)
-    # save image file (temp)
-    imagefile.save(filename)
-    print(filename)
-    a = None
-    with open(filename, "rb") as image:
-        f = image.read()
-        # save image file
-        a = fs.put(f)
 
-    if len(list(db.forms.find({"uid": userID}))) == 0:
-        data = {
-            'special': 'true',
-            'images': [a],
-            'uid': userID
-        }
-        db.forms.insert_one(data)
-    else:
-        userProfile = list(db.forms.find({"uid": userID}))[0]
-        db.forms.delete_one({"uid": userID})
-        userProfile['images'].append(a)
-        db.forms.insert_one(userProfile)
+# @app.route('/uploadImage', methods=['GET', 'POST'])
+# def uploadImage():
+#     userID = request.args.get('uid')
 
-    print('done')
-    return "Image Uploaded Successfully"
+#     # retrieve file from request params
+#     imagefile = request.files['image']
+#     # retrieve filename from file
+#     filename = werkzeug.utils.secure_filename(imagefile.filename)
+#     # save image file (temp)
+#     imagefile.save(filename)
+#     print(filename)
+#     a = None
+#     with open(filename, "rb") as image:
+#         f = image.read()
+#         # save image file
+#         a = fs.put(f)
 
-@app.route('/uploadAudio', methods=['GET', 'POST'])
-def uploadAudio():
-    userID = request.args.get('uid')
+#     if len(list(db.forms.find({"uid": userID}))) == 0:
+#         data = {
+#             'special': 'true',
+#             'images': [a],
+#             'uid': userID
+#         }
+#         db.forms.insert_one(data)
+#     else:
+#         userProfile = list(db.forms.find({"uid": userID}))[0]
+#         db.forms.delete_one({"uid": userID})
+#         userProfile['images'].append(a)
+#         db.forms.insert_one(userProfile)
 
-    # retrieve file from request params
-    audiofile = request.files['audio']
-    # retrieve filename from file
-    filename = werkzeug.utils.secure_filename(audiofile.filename)
-    # save audio file (temp)
-    auiofile.save(filename)
-    print(filename)
-    return "Audio File Uploaded Successfully"
+#     print('done')
+#     return "Image Uploaded Successfully"
+
+# @app.route('/uploadAudio', methods=['GET', 'POST'])
+# def uploadAudio():
+#     userID = request.args.get('uid')
+
+#     # retrieve file from request params
+#     audiofile = request.files['audio']
+#     # retrieve filename from file
+#     filename = werkzeug.utils.secure_filename(audiofile.filename)
+#     # save audio file (temp)
+#     auiofile.save(filename)
+#     print(filename)
+#     return "Audio File Uploaded Successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
