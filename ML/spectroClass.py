9a5dad7c7331cdb30ef425729591a6d4cb9fda02
miro5460 <robin411989@gmail.com>
2019-10-31 23:36:26 -0400

    created class for wav to spectro conversion

---
+++ ML/spectroClass.py
@@ -0,0 +1,15 @@
+import os
+import ffmpy
+
+def convertAudio(self):
+    myFile = os.fsdecode(self)
+    if myFile.endswith(".wav"):
+        ff = ffmpy.FFmpeg(inputs={myFile: None}, outputs={myFile + '.jpg': ['-lavfi', 'showspectrumpic=s=1024x512:legend=disabled']})
+        ff.run()
+    else:
+        raise Exception("File was not in .wav format")
+    return 
+
+class audioProcessing:
+    def __init__(self, audioFile):
+        self.audioFile = audioFile
