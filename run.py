1f1b868595994df51250423234ad35375ba5d57d
Lawrence Chan <chanlawrencet@gmail.com>
2019-11-05 08:43:55 -0500

    asdf

--- run.py
+++ run.py
@@ -35,6 +35,11 @@ def getImages():
     print(request.args)
     userID = request.args.get('uid')
     imageNumber = int(request.args.get('number'))
+    multi_dict = request.args
+    for key in multi_dict:
+        print(multi_dict.get(key))
+        print(multi_dict.getlist(key))
+
     if len(list(db.forms.find({"uid": userID}))) == 0:
         return str({'imageIDs': []})
     else:
